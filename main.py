66719e3a16bdcfb21d67c82cf54747d13338284d
Manthan Sawant <61893709+hawko666@users.noreply.github.com>
2022-05-01 10:46:21 +0000

    written in comment

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+#print("hello world")
+#this is comment
+#print("hello\nworld")
+
+#this is multiline string
+print("""hello
+      
+      jasjhl
+      fahsdkj;k""")
+#concadinating
+print("hello" + " World")
+
+#multiple string
+print("hello" * 10)
